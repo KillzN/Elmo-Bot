7900ac9fc30bca543d77ca668022b3c26c7da90d
KillzN <scorpionsheep203@gmail.com>
2024-11-02 19:24:20 -0300

    KILL BOT

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ import moment from 'moment-timezone'
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.owner = [
-  ['56983073328', 'KillzN', true],
-  ['56983073328'],
-  ['56983073328'],
-  ['56983073328']
+  ['56993761200', 'Elmo', true],
+  ['56993761200'],
+  ['56993761200'],
+  ['56993761200']
 ]
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
@@ -23,13 +23,13 @@ global.prems = []
    
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = `𝗞𝗜𝗟𝗟 𝗕𝗢𝗧`
-global.author = '{\n "𝗕𝗢𝗧": {\n   "name": 𝗞𝗜𝗟𝗟 𝗕𝗢𝗧",\n     "author": "Anuar",\n   "status_bot": "active"\n }\n}'
+global.packname = `𝐄𝐋𝐌𝐎 𝐁𝐎𝐓`
+global.author = '{\n "𝗕𝗢𝗧": {\n   "name": 𝐄𝐋𝐌𝐎 𝐁𝐎𝐓",\n     "author": "Anuar",\n   "status_bot": "active"\n }\n}'
 global.wait = 'KILL  ᡣ𐭩'
-global.botname = '𝗞𝗜𝗟𝗟 𝗕𝗢𝗧  ᡣ𐭩'
-global.textbot = `@ineffable.mvrco`
+global.botname = '𝐄𝐋𝐌𝐎 𝐁𝐎𝐓  ᡣ𐭩'
+global.textbot = `@botelmo_`
 global.listo = 'Completado ✯'
-global.namechannel = '𝗞𝗜𝗟𝗟 𝗕𝗢𝗧'
+global.namechannel = '𝐄𝐋𝐌𝐎 𝐁𝐎𝐓'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
@@ -38,12 +38,12 @@ global.miniurl = fs.readFileSync('./storage/img/siskedurl.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.group = 'https://chat.whatsapp.com/FFB0fgdK3IP4ZEmZ8jF1CC'
-global.canal = 'https://chat.whatsapp.com/FFB0fgdK3IP4ZEmZ8jF1CC'
+global.group = 'instagram.com/botelmo_'
+global.canal = 'instagram.com/botelmo_'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "56983073328-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "56993761200-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: botname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://i.ibb.co/qFTp64d/KILLZN-min.png`)).buffer()
+let img = await (await fetch(`https://i.ibb.co/C0YF1kC/Elmo.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 let txt = `*Hola!, te invito a unirte a mis grupos de ventas*
